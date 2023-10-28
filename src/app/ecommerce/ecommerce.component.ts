ac71b24b05f20cdbe669dba24bb053f61175a7f3
Hkzen <hakuzen72@gmail.com>
2023-10-28 09:00:58 +0700

    uts

---
+++ src/app/ecommerce/ecommerce.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ecommerce',
+  templateUrl: './ecommerce.component.html',
+  styleUrls: ['./ecommerce.component.css'],
+})
+export class EcommerceComponent {
+  nama = 'Ibnu Madjah';
+  nim = '212102417';
+}
